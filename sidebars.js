f5d9537de50f94a46dc34e14454d234c3c98b924
Christian Elbrianno <crse@protonmail.ch>
2021-05-26 16:51:04 +0700

    hideable sidebar

--- sidebars.js
+++ sidebars.js
@@ -2,4 +2,8 @@ module.exports = {
   someSidebar: {
     JKT48: ['faq', 'kamus', 'fanbase', 'sosmed-restruk', 'rh2021', 'ava', 'archive'],
   },
+  themeConfig: {
+    // highlight-starrt
+    hideableSidebar: true,
+  },
 };
