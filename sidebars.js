f118b9a82a10445404f4e1798b9d1e281c8bc8ca
Christian Elbrianno <crse@protonmail.ch>
2021-05-26 16:57:28 +0700

    Revert "hideable sidebar"
    This reverts commit f5d9537de50f94a46dc34e14454d234c3c98b924.

--- sidebars.js
+++ sidebars.js
@@ -2,8 +2,4 @@ module.exports = {
   someSidebar: {
     JKT48: ['faq', 'kamus', 'fanbase', 'sosmed-restruk', 'rh2021', 'ava', 'archive'],
   },
-  themeConfig: {
-    // highlight-starrt
-    hideableSidebar: true,
-  },
 };
