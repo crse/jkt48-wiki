a4ecc19c0b51f06bc206e819e7497a9c29ade599
Christian Elbrianno <crse@protonmail.ch>
2021-07-13 17:59:26 +0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,7 @@
 module.exports = {
   someSidebar: {
-    JKT48: ['faq', 'kamus', 'fanbase', 'sosmed-restruk', 'rh2021', 'ava'],
+    JKT48: ['faq', 'kamus'],
+    Katalog: ['fanbase', 'ava'],
+    Arsip: ['sosmed-restruk', 'rh2021']
   },
 };
